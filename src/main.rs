4db8187e92a3c58b7a462bff79da3d50b1037cf0
Chenfan Zhang <zcfqq2@163.com>
2025-04-22 21:46:47 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use converter_rust::mcz2osz;
 
 fn main() -> std::io::Result<()>{
-    mcz2osz::process_whole_dir_mcz()
-    // webapp::main()
+    // mcz2osz::process_whole_dir_mcz()
+    webapp::main()
 }
